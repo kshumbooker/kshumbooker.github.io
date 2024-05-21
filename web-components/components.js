6aaf850404e45839a3f1a0fe24e534b4f2319a3c
Kenneth Shum <140402252+kshumbooker@users.noreply.github.com>
2024-05-21 18:43:14 +0100

    Update components.js

--- web-components/components.js
+++ web-components/components.js
@@ -527,12 +527,16 @@ class HeroBannerCarousel extends HTMLElement {
       </div>
       `).join('')}
     </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#HeroBannerCarousel" data-bs-slide="prev" style="background: ${this.arrowBgColor}; opacity: 1;">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <button class="carousel-control-prev" type="button" data-bs-target="#HeroBannerCarousel" data-bs-slide="prev" style="background: ${this.arrowBgColor}">
+    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" class="bi bi-arrow-left-circle-fill" viewBox="0 0 16 16">
+    <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0m3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z"/>
+  </svg>
       <span class="visually-hidden">Previous</span>
     </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#HeroBannerCarousel" data-bs-slide="next" style="background: ${this.arrowBgColor}; opacity: 1;">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <button class="carousel-control-next" type="button" data-bs-target="#HeroBannerCarousel" data-bs-slide="next" style="background: ${this.arrowBgColor}">
+      <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
+        <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
+      </svg>
       <span class="visually-hidden">Next</span>
     </button>
   </div>
@@ -1083,13 +1087,14 @@ class Button extends HTMLElement {
 
 class HeroImage extends HTMLElement {
   template = () => `
-    <img src="${this.image}" class="img-fluid" alt="${this.alt}" />
+    <a href="${this.url}"><img src="${this.image}" class="img-fluid" alt="${this.alt}" /></a>
   `;
 
   constructor() {
     super();
     this.image = 'https://static.thcdn.com/images/xlarge/webp/widgets/190-en/19/original-ABBA_Brand_Page_Banner_1-012219.png';
     this.alt = 'Absolut Vodka';
+    this.url = 'https://www.yourcoca-cola.co.uk/brands/absolut-vodka-sprite.list';
     this.render();
   }
 
