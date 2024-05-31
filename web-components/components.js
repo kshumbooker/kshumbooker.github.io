2bf5bdfa20b71bec13e4886795387eb1f9edb878
Kenneth Shum <140402252+kshumbooker@users.noreply.github.com>
2024-05-31 16:53:19 +0100

    Update components.js

--- web-components/components.js
+++ web-components/components.js
@@ -1103,31 +1103,6 @@ class SubPageNavigation extends HTMLElement {
   }
 }
 
-/*class StandardCopy extends HTMLElement {
-  template = () => `
-    <p class="mb-4" style="font-size: ${this.headingSize}; color: ${this.headingColor}">${this.heading}</p>
-    <p style="font-size: ${this.textSize}; color: ${this.textColor}">${this.text}</p>
-  `;
-
-
-  constructor() {
-    super();
-    this.heading = 'Shop your favourites';
-    this.headingSize = '36px';
-    this.headingColor = '#404040';
-    this.text = 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.';
-    this.textSize = '14px';
-    this.textColor = '#000000';
-    this.render();
-  }
-
-  render() {
-    this.innerHTML = `
-      ${this.template().trim()}
-    `;
-  }
-}*/
-
 class StandardCopy extends HTMLElement {
   constructor() {
     super();
