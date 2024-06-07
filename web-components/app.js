3a2454ed2d5d476c23dea71473b98c5510044f26
Kenneth Shum <140402252+kshumbooker@users.noreply.github.com>
2024-06-07 14:46:28 +0100

    Add files via upload

---
+++ web-components/app.js
@@ -0,0 +1,338 @@
+
+      const productCategories = [
+  {
+    "category": "Coca Cola Original Taste - Glass Bottles",
+    "url": "https://www.yourcoca-cola.co.uk/coca-cola-original-taste-24-x-330ml-glass-bottles/12657918.html"
+  },
+  {
+    "category": "Fanta Orange Zero",
+    "url": "https://www.yourcoca-cola.co.uk/fanta-orange-zero-24-x-330ml/12752523.html"
+  },
+  {
+    "category": "Diet Coke",
+    "url": "https://www.yourcoca-cola.co.uk/diet-coke-24-x-330ml/12657929.html"
+  },
+  {
+    "category": "Monster Energy Drink Ultra Peachy Keen",
+    "url": "https://www.yourcoca-cola.co.uk/monster-energy-drink-ultra-12-x-500ml/12657943.html"
+  },
+  {
+    "category": "Relentless Fruit Punch",
+    "url": "https://www.yourcoca-cola.co.uk/relentless-fruit-punch-energy-drink-12-x-500ml/15355387.html"
+  },
+  {
+    "category": "Powerade Golden Mango",
+    "url": "https://www.yourcoca-cola.co.uk/powerade-golden-mango-12-x-500ml/15213056.html"
+  },
+  {
+    "category": "Coca Cola Lemon",
+    "url": "https://www.yourcoca-cola.co.uk/coca-cola-lemon-24-x-330ml/15227578.html"
+  },
+  {
+    "category": "Reign Razzle Berry",
+    "url": "https://www.yourcoca-cola.co.uk/reign-razzle-berry-12-x-500ml/12918403.html"
+  }
+];
+
+const heroBannerCarousel = [
+  {
+    image: 'https://static.thcdn.com/images/xlarge/webp/widgets/190-en/49/original-New_Fanta_Desktop-011449.png',
+    alt: 'Fanta',
+    url: 'https://www.yourcoca-cola.co.uk/brands/fanta.list'
+  },
+  {
+    image: 'https://static.thcdn.com/images/xlarge/webp/widgets/190-en/09/Coca-cola-banner-1920x700-073909.png',
+    alt: 'Coca Cola',
+    url: 'https://www.yourcoca-cola.co.uk/brands/coca-cola-original-taste.list'
+  },
+  {
+    image: 'https://static.thcdn.com/images/xlarge/webp/widgets/190-en/19/original-ABBA_Brand_Page_Banner_1-012219.png',
+    alt: 'Absolut Vodka',
+    url: 'https://www.yourcoca-cola.co.uk/brands/absolut-vodka-sprite.list'
+  },
+  {
+    image: 'https://static.thcdn.com/images/xlarge/webp/widgets/190-en/00/Appletiser-banner-1920x700-080600.png',
+    alt: 'Appletiser',
+    url: 'https://www.yourcoca-cola.co.uk/brands/appletiser.list'
+  },
+  {
+    image: 'https://static.thcdn.com/images/xlarge/webp/widgets/190-en/45/original-Dr_Pepper_Brands_Page_Desktop-091945.png',
+    alt: 'Dr Pepper',
+    url: 'https://www.yourcoca-cola.co.uk/brands/dr-pepper.list'
+  },
+  {
+    image: 'https://static.thcdn.com/images/xlarge/webp/widgets/190-en/35/original-Desktop-031535.png',
+    alt: 'Jack Daniels',
+    url: 'https://www.yourcoca-cola.co.uk/brands/jack-daniels-coca-cola.list'
+  },
+  {
+    image: 'https://static.thcdn.com/images/xlarge/webp/widgets/190-en/00/original-Desktop_%2831%29-025700.png',
+    alt: 'Lilt',
+    url: 'https://www.yourcoca-cola.co.uk/brands/lilt.list'
+  },
+  {
+    image: 'https://static.thcdn.com/images/xlarge/webp/widgets/190-en/14/original-Sprite_1920x586px-020414.png',
+    alt: 'Sprite',
+    url: 'https://www.yourcoca-cola.co.uk/brands/sprite.list' 
+  }
+];
+
+const productsData = [
+  {
+    title: 'Coca-Cola Zero Sugar',
+    volume: '24 x 330ml',
+    image: 'https://static.thcdn.com/images/large/webp//productimg/1600/1600/12657922-9294861134854115.jpg',
+    midascode: 100001,
+    brand: 'coca-cola',
+    price: 13.09,
+    rrp: 1.39,
+    por: 49.7,
+    quantity: 0
+  },
+  {
+    title: 'Coca-Cola Lemon',
+    volume: '24 x 330ml',
+    image: 'https://static.thcdn.com/images/large/webp//productimg/1600/1600/15227578-8645125050786129.jpg',
+    midascode: 100002,
+    brand: 'coca-cola',
+    price: 13.99,
+    rrp: 1.39,
+    por: 55.5,
+    quantity: 0
+  },
+  {
+    title: 'Coca-Cola Original',
+    volume: '24 x 250ml',
+    image: 'https://static.thcdn.com/images/large/webp//productimg/1600/1600/13869129-4134964590200453.jpg',
+    midascode: 100003,
+    brand: 'coca-cola',
+    price: 13.99,
+    rrp: 1.39,
+    por: 55.5,
+    quantity: 0
+  },
+  {
+    title: 'Coca-Cola Cherry',
+    volume: '24 x 330ml',
+    image: 'https://static.thcdn.com/images/large/webp//productimg/1600/1600/12657920-1654899330330921.jpg',
+    midascode: 100004,
+    brand: 'coca-cola',
+    price: 13.99,
+    rrp: 1.39,
+    por: 55.5,
+    quantity: 0
+  },
+  {
+    title: 'Coca-Cola Original Taste Glass Bottles',
+    volume: '24 x 330ml',
+    image: 'https://static.thcdn.com/images/large/webp//productimg/1600/1600/12657918-9414807812038433.jpg',
+    midascode: 100005,
+    brand: 'coca-cola',
+    price: 13.99,
+    rrp: 1.39,
+    por: 55.5,
+    quantity: 0
+  },
+  {
+    title: 'Coca-Cola Zero Sugar Lemon',
+    volume: '24 x 330ml',
+    image: 'https://static.thcdn.com/images/large/webp//productimg/1600/1600/15227577-1255125050706956.jpg',
+    midascode: 100006,
+    brand: 'coca-cola',
+    price: 14.99,
+    rrp: 1.49,
+    por: 57.5,
+    quantity: 0
+  },
+  {
+    title: 'Coca-Cola Zero Sugar Cherry',
+    volume: '24 x 330ml',
+    image: 'https://static.thcdn.com/images/large/webp//productimg/1600/1600/12912901-1414889184313857.jpg',
+    midascode: 100007,
+    brand: 'coca-cola',
+    price: 15.99,
+    rrp: 1.59,
+    por: 58.5,
+    quantity: 0
+  },
+  {
+    title: 'Costa Coffee Latte',
+    volume: '12 x 250ml',
+    image: 'https://static.thcdn.com/images/large/webp//productimg/1600/1600/12657928-1344965615047130.jpg',
+    midascode: 100008,
+    brand: 'costa coffee',
+    price: 15.99,
+    rrp: 1.59,
+    por: 58.5,
+    quantity: 0
+  },
+  {
+    title: 'Costa Coffee Vanilla Latte',
+    volume: '12 x 250ml',
+    image: 'https://static.thcdn.com/images/large/webp//productimg/1600/1600/12882462-1564965615215088.jpg',
+    midascode: 100009,
+    brand: 'costa coffee',
+    price: 15.99,
+    rrp: 1.59,
+    por: 58.5,
+    quantity: 0
+  },
+  {
+    title: 'Costa Coffee Caramel Latte',
+    volume: '12 x 250ml',
+    image: 'https://static.thcdn.com/images/large/webp//productimg/1600/1600/12657927-1074965615454608.jpg',
+    midascode: 100010,
+    brand: 'costa coffee',
+    price: 15.99,
+    rrp: 1.59,
+    por: 58.5,
+    quantity: 0
+  },
+  {
+    title: 'Costa Coffee Flat White',
+    volume: '12 x 250ml',
+    image: 'https://static.thcdn.com/images/large/webp//productimg/1600/1600/12882463-3574965914586723.jpg',
+    midascode: 100011,
+    brand: 'costa coffee',
+    price: 15.99,
+    rrp: 1.59,
+    por: 58.5,
+    quantity: 0
+  },
+  {
+    title: 'Jack Daniel\'s and Coca-Cola',
+    volume: '12 x 330ml',
+    image: 'https://static.thcdn.com/images/large/webp//productimg/1600/1600/14509056-2415036628891027.jpg',
+    midascode: 100012,
+    brand: 'jack daniels',
+    price: 15.99,
+    rrp: 1.59,
+    por: 58.5,
+    quantity: 0
+  },
+  {
+    title: 'Jack Daniel\'s and Coca-Cola Zero',
+    volume: '12 x 330ml',
+    image: 'https://static.thcdn.com/images/large/webp//productimg/1600/1600/14509057-9895036629017087.jpg',
+    midascode: 100013,
+    brand: 'jack daniels',
+    price: 15.99,
+    rrp: 1.59,
+    por: 58.5,
+    quantity: 0
+  },
+  {
+    title: 'Relentless Fruit Punch Energy Drink',
+    volume: '12 x 500ml',
+    image: 'https://static.thcdn.com/images/large/webp//productimg/960/960/15355387-8795143410513525.jpg',
+    midascode: 100014,
+    brand: 'relentless',
+    price: 11.99,
+    rrp: 1.99,
+    por: 41.5,
+    quantity: 0
+  },
+  {
+    title: 'Relentless Raspberry Zero Sugar Energy Drink',
+    volume: '12 x 500ml',
+    image: 'https://static.thcdn.com/images/large/webp//productimg/960/960/13733909-2774951400808045.jpg',
+    midascode: 100015,
+    brand: 'relentless',
+    price: 12.99,
+    rrp: 1.89,
+    por: 40.5,
+    quantity: 0
+  },
+  {
+    title: 'Relentless Origin Energy Drink',
+    volume: '12 x 500ml',
+    image: 'https://static.thcdn.com/images/large/webp//productimg/960/960/13220383-1204886545310409.jpg',
+    midascode: 100016,
+    brand: 'relentless',
+    price: 13.99,
+    rrp: 1.79,
+    por: 44.5,
+    quantity: 0
+  },
+  {
+    title: 'Relentless Peach Zero Sugar Energy Drink',
+    volume: '12 x 500ml',
+    image: 'https://static.thcdn.com/images/large/webp//productimg/960/960/13733910-9564951400918733.jpg',
+    midascode: 100017,
+    brand: 'relentless',
+    price: 13.79,
+    rrp: 1.83,
+    por: 45.5,
+    quantity: 0
+  },
+  {
+    title: 'Relentless Cherry Energy Drink',
+    volume: '12 x 500ml',
+    image: 'https://static.thcdn.com/images/large/webp//productimg/960/960/13220386-3294886545986861.jpg',
+    midascode: 100018,
+    brand: 'relentless',
+    price: 13.49,
+    rrp: 1.89,
+    por: 45.8,
+    quantity: 0
+  },
+  {
+    title: 'Relentless Passion Punch Energy Drink',
+    volume: '12 x 500ml',
+    image: 'https://static.thcdn.com/images/large/webp//productimg/960/960/13220384-1685004239058525.jpg',
+    midascode: 100019,
+    brand: 'relentless',
+    price: 13.19,
+    rrp: 1.93,
+    por: 46.8,
+    quantity: 0
+  }
+];
+
+const subPageNavigation = [
+  {
+    id: 'home',
+    title: 'Home',
+    text: 'Limited-Time Offer: Place an order worth £60+ and choose a Free Gift OR Free Entry to our Prize Draw for a Chance To Win £250 of account credit, which you can use to buy your favourite drinks from Your Coca-Cola. Offer ends 19/05/24. Gifts available while stocks last.',
+    image: 'https://static.thcdn.com/images/medium/webp/widgets/190-en/17/original-Free_Gift_Mobile-073417.png',
+    color: '#000000'
+  },
+  {
+    id: 'monster',
+    title: 'Monster Energy',
+    text: 'Tear into a can of Monster Energy wherever your journey takes you, the meanest energy drink on the planet. The Monster Energy blend combined with caffeine gives you the energy you need in a smooth easy drinking flavour. Athletes, gamers, musicians, students, road warriors, metal heads, geeks, hipsters, and bikers dig it - you will too. Unleash the Beast!',
+    image: 'https://static.thcdn.com/images/medium/webp/widgets/190-en/14/Monster-header-375x375-113914.png',
+    color: '#121212'
+  },
+  {
+    id: 'cocacola',
+    title: 'Coca Cola',
+    text: 'Coca‑Cola history began in 1886 when Dr. John Pemberton created a distinctive soft drink now known as Coca-Cola Original Taste. Today, you can find Coca-Cola Original Taste in a variety of sizes to suit every lifestyle and occasion.',
+    image: 'https://static.thcdn.com/images/medium/webp/widgets/190-en/29/Coca-cola-header-375x375-090329.png',
+    color: '#C70100'
+  },
+  {
+    id: 'fanta',
+    title: 'Fanta',
+    text: 'Bright, bubbly, instantly refreshing and great tasting. Fanta is made with 100% natural flavours and is caffeine free. Fanta is available in a variety of real fruit flavours.',
+    image: 'https://static.thcdn.com/images/medium/webp/widgets/190-en/58/original-New_Fanta_Mobile-011458.png',
+    color: '#F86C02'
+  }
+];
+
+    const productsByBrand = (brand) => {
+      return productsData.filter(p => p.brand == brand)
+    }
+
+
+    let productCategoriesContainer = document.getElementById("product-categories");
+    let heroBannerCarouselContainer = document.getElementById("hero-banner-carousel");
+    let productCardListContainer = document.getElementById('product-card-list');
+    let subPageNavigationContainer = document.getElementById('sub-page-navigation');
+    let productCardListCarouselContainer = document.getElementById('product-card-list-carousel');
+    productCategoriesContainer.setAttribute("data-categories", JSON.stringify(productCategories));
+    heroBannerCarouselContainer.setAttribute("data-hero-banner-carousel", JSON.stringify(heroBannerCarousel));
+    productCardListContainer.setAttribute("data-product-card-list", JSON.stringify(productsByBrand(productCardListContainer.getAttribute('data-brand'))));
+    subPageNavigationContainer.setAttribute('data-sub-page-navigation', JSON.stringify(subPageNavigation));
+    productCardListCarouselContainer.setAttribute('data-product-card-list-carousel', JSON.stringify(productsByBrand(productCardListCarouselContainer.getAttribute('data-brand'))));
+   
