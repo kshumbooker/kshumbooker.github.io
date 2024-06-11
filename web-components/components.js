2dcfe562bd4f6392b8f17d0e894465a9f4c40a10
Kenneth Shum <140402252+kshumbooker@users.noreply.github.com>
2024-06-11 14:10:50 +0100

    Update components.js

--- web-components/components.js
+++ web-components/components.js
@@ -380,7 +380,7 @@ class HeroBannerA extends HTMLElement {
         }
       }
     </style>
-    <div class="container justify-content-center mt-5 mb-5">
+    <div class="container justify-content-center text-center mt-5 mb-5">
       <img src="${this.getAttribute('data-image')}" class="img-fluid" />
         <a class="btn btn-light mt-3 hero-banner-a-button" href="${this.getAttribute('data-url')}">${this.getAttribute('data-text')}</a>
     </div>
