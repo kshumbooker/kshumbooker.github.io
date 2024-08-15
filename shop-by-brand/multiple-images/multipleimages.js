47307e284b5682eaf16ca129f50a43ed0d7fd037
Kenneth Shum <140402252+kshumbooker@users.noreply.github.com>
2024-08-15 21:43:27 +0100

    Update multipleimages.js

--- shop-by-brand/multiple-images/multipleimages.js
+++ shop-by-brand/multiple-images/multipleimages.js
@@ -1,5 +1,9 @@
 
     let images = [
+      {
+        shotTypeId: 60,
+        path: 'https://media.booker.co.uk/bbimages/1/e2c1ba87-1b10-461b-bb9e-98c5e031e7f0.jpg?date=20240806'
+      },
     {
         shotTypeId: 1,
             path: 'https://media.booker.co.uk/bbimages/1/7d5d4b2e-e4d1-4e0e-9ce7-197938e3b990.jpg?date=20240808'
@@ -7,10 +11,6 @@
     {
         shotTypeId: 3,
             path: 'https://media.booker.co.uk/bbimages/1/d8996462-83ac-4b81-b1a6-7a51ae69a2df.jpg?date=20240808'
-          },
-          {
-            shotTypeId: 4,
-            path: 'https://media.booker.co.uk/bbimages/1/17f8c7d1-291f-4eaf-9df8-08819467efbd.jpg?date=20240808'
           },
           {
             shotTypeId: 10,
@@ -21,10 +21,12 @@
             path: 'https://media.booker.co.uk/bbimages/1/2f2b32f2-1059-4d37-9bca-eaca5f0cafc7.jpg?date=20240808'
           },
           {
-            shotTypeId: 60,
-            path: 'https://media.booker.co.uk/bbimages/1/e2c1ba87-1b10-461b-bb9e-98c5e031e7f0.jpg?date=20240806'
+            shotTypeId: 4,
+            path: 'https://media.booker.co.uk/bbimages/1/17f8c7d1-291f-4eaf-9df8-08819467efbd.jpg?date=20240808'
           }
         ];
+
+        images.sort((a, b) => a.shotTypeId - b.shotTypeId);
     
         images.map(image => {
             document.getElementById('multipleImagesPanel').innerHTML += `<a title="" href="${image.path}" data-zoom-id="product" data-image="${image.path}">
