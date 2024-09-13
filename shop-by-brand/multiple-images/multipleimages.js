27d9602235912882d710ba963a5a2b285d3e026b
Kenneth Shum <140402252+kshumbooker@users.noreply.github.com>
2024-09-13 14:47:42 +0100

    Update multipleimages.js

--- shop-by-brand/multiple-images/multipleimages.js
+++ shop-by-brand/multiple-images/multipleimages.js
@@ -1,34 +1,34 @@
 
     let images = [
       {
-        title: 'Walkers Crisps Ready Salted 32.5g',
+        title: 'Skips Prawn Cocktail Crisps 45g £1.25 PMP',
         shotTypeId: 60,
-        path: 'https://media.booker.co.uk/bbimages/1/e2c1ba87-1b10-461b-bb9e-98c5e031e7f0.jpg?date=20240806'
+        path: 'https://media.booker.co.uk/bbimages/1/26d1a4e8-6734-455d-abd2-c0a1d5aa8d3d.jpg'
       },
       {
         title: 'Walkers Crisps Ready Salted 32.5g',
         shotTypeId: 1,
-        path: 'https://media.booker.co.uk/bbimages/1/7d5d4b2e-e4d1-4e0e-9ce7-197938e3b990.jpg?date=20240808'
+        path: 'https://media.booker.co.uk/bbimages/1/7d5d4b2e-e4d1-4e0e-9ce7-197938e3b990.jpg'
       },
       {
-        title: 'Walkers Crisps Ready Salted 32.5g',
+        title: 'Walkers Crisps Salt & Vinegar 32.5g',
         shotTypeId: 3,
-        path: 'https://media.booker.co.uk/bbimages/1/d8996462-83ac-4b81-b1a6-7a51ae69a2df.jpg?date=20240808'
+        path: 'https://media.booker.co.uk/bbimages/1/42f34b12-b75c-4611-895d-cfffe2bc7932.jpg'
       },
       {
-        title: 'Walkers Crisps Ready Salted 32.5g',
+        title: 'Walkers Wotsits Cheese Snacks Crisps £1.25 RRP PMP 60g',
         shotTypeId: 10,
-        path: 'https://media.booker.co.uk/bbimages/1/f2b51c83-aab3-4f8a-8518-4da47d8901a0.jpg?date=20240807'
+        path: 'https://media.booker.co.uk/bbimages/1/1bc52403-f4e1-42a1-a873-d12c20f872d5.jpg'
       },
       {
-        title: 'Walkers Crisps Ready Salted 32.5g',
+        title: 'Nik Naks Nice N Spicy Crisps 75g, £1.25 PMP',
         shotTypeId: 31,
-        path: 'https://media.booker.co.uk/bbimages/1/2f2b32f2-1059-4d37-9bca-eaca5f0cafc7.jpg?date=20240808'
+        path: 'https://media.booker.co.uk/bbimages/1/388701c1-f5a9-4d88-b162-74386daca7ea.jpg'
       },
       {
-        title: 'Walkers Crisps Ready Salted 32.5g',
+        title: 'Walkers Crisps Cheese & Onion 32.5g',
         shotTypeId: 4,
-        path: 'https://media.booker.co.uk/bbimages/1/17f8c7d1-291f-4eaf-9df8-08819467efbd.jpg?date=20240808'
+        path: 'https://media.booker.co.uk/bbimages/1/11212c08-1d10-48f0-9556-53fbe2c7b703.jpg'
       }
     ];
 
@@ -41,25 +41,25 @@
 
     const brandbankImages = document.querySelectorAll('.multipleImagesPanel > a');
 
-
     [...brandbankImages].map((b, i, arr) => {
       if (arr.length > 5 && i == 5) {
         b.classList.add('d-none');
       }
-       b.addEventListener('click', () => {clickCarouselImg(b, i, arr)});
-        b.addEventListener('touchstart', () => {clickCarouselImg(b, i, arr)});
+      b.addEventListener('click', () => {clickCarouselImg(b, i, arr)});
+      b.addEventListener('touchstart', () => {clickCarouselImg(b, i, arr)});
     });
 
     const multiImagesSliders = document.querySelectorAll('.multi-icon > a');
 
     [...multiImagesSliders].map(m => {
       m.addEventListener('click', () => {slide(m)});
+      m.addEventListener('touchstart', () => {slide(m)});
     });
 
     document.querySelector('.multi-icon.left').classList.add('disabled');
 
     const clickCarouselImg = (b, index, brandbankImages) => {
-     activeSliders(index, brandbankImages);
+      activeSliders(index, brandbankImages);
     }
     
     const slide = (s) => {
@@ -74,14 +74,12 @@
       [...mzThumbSelected].map((mz, index) => {
         if (mz.classList.contains('mz-thumb-selected')) {
           mz.classList.remove('mz-thumb-selected');
-          
           activeKey = (direction == 'next' && index < mzThumbSelected.length) ? index + 1 : (direction == 'prev' && index > 0) ? index - 1 : false;     
         }
 
         activeSliders(activeKey, mzThumbSelected);
-      });     
+      });
 
-      
       mzThumbSelected[activeKey].classList.add('mz-thumb-selected');
 
       let currentImgUrl = mzThumbSelected[activeKey].href;
@@ -89,6 +87,7 @@
     }
 
     const activeSliders = (index, thumbs) => {
+     
       if (thumbs.length > 5 && index >= 4) {
         thumbs[0].classList.add('d-none');
         thumbs[5].classList.remove('d-none');
@@ -96,7 +95,6 @@
         thumbs[5].classList.add('d-none');
         thumbs[0].classList.remove('d-none');
       }
-
 
       if (index == 0) {
         document.querySelector('.multi-icon.left').classList.add('disabled');
@@ -107,4 +105,3 @@
         document.querySelector('.multi-icon.right').classList.remove('disabled');
       }
     }
-
