17f6df75853109f17074edeff7e5389859c14c24
KShum <140402252+kshumbooker@users.noreply.github.com>
2024-11-20 13:23:55 +0000

    Update app.js

--- custom-tools/file-converter/app.js
+++ custom-tools/file-converter/app.js
@@ -164,7 +164,8 @@ let convertedValues = [];
 
 			let objUrl = URL.createObjectURL(newFile);
 
-			let csvFileLink = document.createElement('a')
+			let csvFileLink = document.createElement('a');
+			csvFileLink.setAttribute('class', 'focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-3xl text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800');
 			csvFileLink.setAttribute('href', objUrl);
 			csvFileLink.setAttribute('download', file.name + 'ConvertedFile.csv');
 			csvFileLink.textContent = 'Click to download converted ' + fileType.toUpperCase() + ' file';
